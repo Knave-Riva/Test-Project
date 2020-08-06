d72f62be94459e60c91ed227c0307a2a64681767
Riva <knaveriva@gmail.com>
2020-08-06 11:43:46 +0300

    Start

---
+++ JS/script.js
@@ -0,0 +1,53 @@
+"use strict";
+// console.log("fuck you");
+// let number = 5;
+// const LeftBorderWidth = 1;
+
+// number = 12;
+// console.log(number);
+
+// LeftBorderWidth = 10;
+
+// let obj = {
+//     name: "Sergey",
+//     age: 32,
+//     isMaried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+//alert('Fuck you');
+
+// const res = confirm("Ты мудак?");
+// console.log(res);
+
+// const answer = +prompt("Вам есть 18?", "18");
+//     console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("Введите ваше имя: ", "Имя");
+// answers[1] = prompt("Введите вашу фамилию", "Фамилия");
+// answers[2] = prompt("Введите ваш возраст", "Возраст");
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// const name = ("Seriy");
+
+// alert(`Привет, ${name}`);
+
+// let incr = 10,
+//     decr = 10;
+
+//     // incr++;
+//     // decr--;
+
+//     console.log(++incr);
+//     console.log(--decr);
+
+const isChecked = true,
+      isClose = false;
+      
+console.log(isChecked || isClose);      
